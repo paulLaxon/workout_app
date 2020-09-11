be3ea19eb29522e483c1e9c5343e320c7b2c03bf
Paul Laxon <plaxon@Paul-Laxon.local>
2020-09-11 16:35:32 -0400

    fix sign up spec

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -3,12 +3,16 @@
 require 'rails_helper'
 
 RSpec.feature 'Sign up user:' do
-  scenario 'when a user signs up' do
+  scenario 'when a user signs up with valid credentials' do
     visit '/'
 
     click_link 'Sign up'
+
     fill_in 'Email', with: 'paul@example.com'
     fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+
+    click_button 'Sign up'
 
     expect(page).to have_content('You have signed up successfully')
   end
