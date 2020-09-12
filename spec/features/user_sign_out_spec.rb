a951d034c31692c450956aeb14e8fca26aace086
Paul Laxon <plaxon@Paul-Laxon.local>
2020-09-11 22:44:33 -0400

    implement user sign out

---
+++ spec/features/user_sign_out_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'Sign out user:' do
+  before do
+    @user = User.create!(email: 'test@test.com', password: 'password')
+    visit '/'
+
+    click_link 'Sign in'
+
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+
+    click_button 'Log in'
+  end
+
+  scenario 'when a user signs out' do
+    click_link 'Sign out'
+
+    expect(page).to have_content('Signed out successfully')
+    expect(page).not_to have_content(@user.email)
+    expect(page).to have_no_content('Sign out')
+  end
+end
