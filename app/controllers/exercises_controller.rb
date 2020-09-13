12772a6311e5b165789ddaf1dbd0f9d796c06c7e
Paul Laxon <plaxon@Paul-Laxon.local>
2020-09-12 23:25:54 -0400

    exercise controller and show partial

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -5,5 +5,25 @@ class ExercisesController < ApplicationController
     @exercise = current_user.exercises.new
   end
 
-  def create; end
+  def create
+    @exercise = current_user.exercises.new(exercise_params)
+
+    if @exercise.save
+      flash[:notice] = 'Exercise has been created.'
+      redirect_to [current_user, @exercise]
+    else
+      flash[:alert] = 'Exercise has not been created.'
+      render :new
+    end
+  end
+
+  def show
+    @exercise = current_user.exercises.find(params[:id])
+  end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:duration_in_min, :workout, :workout_date, :user_id)
+  end
 end
