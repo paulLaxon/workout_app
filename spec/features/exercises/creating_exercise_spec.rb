2a7f1721b9492f3ad69ffa6d9a2be677d747ef47
Paul Laxon <plaxon@Paul-Laxon.local>
2020-09-13 11:31:37 -0400

    create exercise unsuccessful

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -6,14 +6,14 @@ RSpec.feature 'Creating exercise:' do
   before do
     @user = User.create!(email: 'user@example.com', password: 'password')
     login_as(@user)
-  end
-  scenario 'with valid inputs' do
     visit '/'
 
     click_link 'My Lounge'
     click_link 'New Workout'
     expect(page).to have_link('Back')
+  end
 
+  scenario 'with valid inputs' do
     fill_in 'Duration', with: 70
     fill_in 'Workout Details', with: 'Weight lifting'
     fill_in 'Activity Date', with: '2020-06-20'
@@ -24,5 +24,14 @@ RSpec.feature 'Creating exercise:' do
     exercise = Exercise.last
     expect(current_path).to eq(user_exercise_path(@user, exercise))
     expect(exercise.user_id).to eq(@user.id)
+  end
+
+  scenario 'with invalid inputs' do
+    click_button 'Create Exercise'
+
+    expect(page).to have_content('Exercise has not been created')
+    expect(page).to have_content('Duration in min is not a number')
+    expect(page).to have_content("Workout details can't be blank")
+    expect(page).to have_content("Activity date can't be blank")
   end
 end
